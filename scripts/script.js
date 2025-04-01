3155f9a15f098c78cf0a6531464e8e74a70a90bb
Jordan <jordinholenhart@gmail.com>
2025-03-31 20:46:55 -0700

    As opposed to getElementById, querySelector must retrieve an HTML element that can be selected by CSS. FOr instance, in this repo, script.js uses querySelector to retrieve all HTML elements with <p> and classes of "container" and "divStuff." The script then console.logs the entire HTML code, shown in the inspect console.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 let titleElement = document.getElementById("title");
 
 console.log(titleElement);
+
+let caption = document.querySelector("p");
+
+console.log(caption);
+
+let container = document.querySelector(".container");
+
+console.log(container);
+
+let divStuff = document.querySelector(".divStuff");
+
+console.log(divStuff);
