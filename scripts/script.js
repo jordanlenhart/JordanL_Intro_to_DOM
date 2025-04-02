cf761c09f284e9d1814cbd834f139481b02bfe58
Jordan <jordinholenhart@gmail.com>
2025-04-01 20:11:56 -0700

    Created code for innerText, CSS Styling, createElement, and .append. createElement creates the concept of a given element in the linked HTML, while append will tie to the an HTML parent element, such as a the <body>. I used it to create a div at the bottom of the page. It has no text, but it can be seen whne inspecting the browser.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
+// getElementById
 let titleElement = document.getElementById("title");
 
 console.log(titleElement);
 
+// querySelector
 let caption = document.querySelector("p");
 
 console.log(caption);
@@ -14,6 +16,26 @@ let divStuff = document.querySelector(".divStuff");
 
 console.log(divStuff);
 
+
+// querySelectorAll
 let cool = document.querySelectorAll("h2");
 
 console.log(cool);
+
+// innerText
+console.log(document.getElementById("inner-text"))
+
+let text = document.getElementById("inner-text");
+text.innerText = "This got changed yo"
+
+console.log(document.getElementById("inner-text"));
+
+// CSS Styling
+
+document.getElementById("title").style.fontSize = "100px";
+
+// Create Element and Append
+
+const div = document.createElement("div");
+
+document.body.append(div);
